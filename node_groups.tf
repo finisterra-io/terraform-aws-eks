397973af428ca5da82d14c5444bb5850035c84a6
xneyder <xneyder@gmail.com>
2023-12-10 11:09:37 -0500

    Fix

--- node_groups.tf
+++ node_groups.tf
@@ -88,7 +88,8 @@ locals {
   node_sg_name   = coalesce(var.node_security_group_name, "${var.cluster_name}-node")
   create_node_sg = var.create && var.create_node_security_group
 
-  node_security_group_id = local.create_node_sg ? aws_security_group.node[0].id : var.node_security_group_id
+  # node_security_group_id = local.create_node_sg ? aws_security_group.node[0].id : var.node_security_group_id
+  node_security_group_id = var.node_security_group_id
 
   node_security_group_rules = {
     ingress_cluster_443 = {
@@ -182,50 +183,50 @@ locals {
   } : k => v if var.node_security_group_enable_recommended_rules }
 }
 
-resource "aws_security_group" "node" {
-  count = local.create_node_sg ? 1 : 0
+# resource "aws_security_group" "node" {
+#   count = local.create_node_sg ? 1 : 0
 
-  name        = var.node_security_group_use_name_prefix ? null : local.node_sg_name
-  name_prefix = var.node_security_group_use_name_prefix ? "${local.node_sg_name}${var.prefix_separator}" : null
-  description = var.node_security_group_description
-  vpc_id      = var.vpc_id
+#   name        = var.node_security_group_use_name_prefix ? null : local.node_sg_name
+#   name_prefix = var.node_security_group_use_name_prefix ? "${local.node_sg_name}${var.prefix_separator}" : null
+#   description = var.node_security_group_description
+#   vpc_id      = var.vpc_id
 
-  tags = merge(
-    var.tags,
-    {
-      "Name"                                      = local.node_sg_name
-      "kubernetes.io/cluster/${var.cluster_name}" = "owned"
-    },
-    var.node_security_group_tags
-  )
+#   tags = merge(
+#     var.tags,
+#     {
+#       "Name"                                      = local.node_sg_name
+#       "kubernetes.io/cluster/${var.cluster_name}" = "owned"
+#     },
+#     var.node_security_group_tags
+#   )
 
-  lifecycle {
-    create_before_destroy = true
-  }
-}
+#   lifecycle {
+#     create_before_destroy = true
+#   }
+# }
 
-resource "aws_security_group_rule" "node" {
-  for_each = { for k, v in merge(
-    local.node_security_group_rules,
-    local.node_security_group_recommended_rules,
-    var.node_security_group_additional_rules,
-  ) : k => v if local.create_node_sg }
+# resource "aws_security_group_rule" "node" {
+#   for_each = { for k, v in merge(
+#     local.node_security_group_rules,
+#     local.node_security_group_recommended_rules,
+#     var.node_security_group_additional_rules,
+#   ) : k => v if local.create_node_sg }
 
-  # Required
-  security_group_id = aws_security_group.node[0].id
-  protocol          = each.value.protocol
-  from_port         = each.value.from_port
-  to_port           = each.value.to_port
-  type              = each.value.type
+#   # Required
+#   security_group_id = aws_security_group.node[0].id
+#   protocol          = each.value.protocol
+#   from_port         = each.value.from_port
+#   to_port           = each.value.to_port
+#   type              = each.value.type
 
-  # Optional
-  description              = lookup(each.value, "description", null)
-  cidr_blocks              = lookup(each.value, "cidr_blocks", null)
-  ipv6_cidr_blocks         = lookup(each.value, "ipv6_cidr_blocks", null)
-  prefix_list_ids          = lookup(each.value, "prefix_list_ids", [])
-  self                     = lookup(each.value, "self", null)
-  source_security_group_id = try(each.value.source_cluster_security_group, false) ? local.cluster_security_group_id : lookup(each.value, "source_security_group_id", null)
-}
+#   # Optional
+#   description              = lookup(each.value, "description", null)
+#   cidr_blocks              = lookup(each.value, "cidr_blocks", null)
+#   ipv6_cidr_blocks         = lookup(each.value, "ipv6_cidr_blocks", null)
+#   prefix_list_ids          = lookup(each.value, "prefix_list_ids", [])
+#   self                     = lookup(each.value, "self", null)
+#   source_security_group_id = try(each.value.source_cluster_security_group, false) ? local.cluster_security_group_id : lookup(each.value, "source_security_group_id", null)
+# }
 
 ################################################################################
 # Fargate Profile

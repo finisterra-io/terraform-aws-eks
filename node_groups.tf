9a5c7b859dd1a45093017abfdc74a715e80ec907
xneyder <xneyder@gmail.com>
2023-12-10 11:24:13 -0500

    Fix

--- node_groups.tf
+++ node_groups.tf
@@ -91,96 +91,96 @@ locals {
   # node_security_group_id = local.create_node_sg ? aws_security_group.node[0].id : var.node_security_group_id
   node_security_group_id = var.node_security_group_id
 
-  node_security_group_rules = {
-    ingress_cluster_443 = {
-      description                   = "Cluster API to node groups"
-      protocol                      = "tcp"
-      from_port                     = 443
-      to_port                       = 443
-      type                          = "ingress"
-      source_cluster_security_group = true
-    }
-    ingress_cluster_kubelet = {
-      description                   = "Cluster API to node kubelets"
-      protocol                      = "tcp"
-      from_port                     = 10250
-      to_port                       = 10250
-      type                          = "ingress"
-      source_cluster_security_group = true
-    }
-    ingress_self_coredns_tcp = {
-      description = "Node to node CoreDNS"
-      protocol    = "tcp"
-      from_port   = 53
-      to_port     = 53
-      type        = "ingress"
-      self        = true
-    }
-    ingress_self_coredns_udp = {
-      description = "Node to node CoreDNS UDP"
-      protocol    = "udp"
-      from_port   = 53
-      to_port     = 53
-      type        = "ingress"
-      self        = true
-    }
-  }
+  # node_security_group_rules = {
+  #   ingress_cluster_443 = {
+  #     description                   = "Cluster API to node groups"
+  #     protocol                      = "tcp"
+  #     from_port                     = 443
+  #     to_port                       = 443
+  #     type                          = "ingress"
+  #     source_cluster_security_group = true
+  #   }
+  #   ingress_cluster_kubelet = {
+  #     description                   = "Cluster API to node kubelets"
+  #     protocol                      = "tcp"
+  #     from_port                     = 10250
+  #     to_port                       = 10250
+  #     type                          = "ingress"
+  #     source_cluster_security_group = true
+  #   }
+  #   ingress_self_coredns_tcp = {
+  #     description = "Node to node CoreDNS"
+  #     protocol    = "tcp"
+  #     from_port   = 53
+  #     to_port     = 53
+  #     type        = "ingress"
+  #     self        = true
+  #   }
+  #   ingress_self_coredns_udp = {
+  #     description = "Node to node CoreDNS UDP"
+  #     protocol    = "udp"
+  #     from_port   = 53
+  #     to_port     = 53
+  #     type        = "ingress"
+  #     self        = true
+  #   }
+  # }
 
-  node_security_group_recommended_rules = { for k, v in {
-    ingress_nodes_ephemeral = {
-      description = "Node to node ingress on ephemeral ports"
-      protocol    = "tcp"
-      from_port   = 1025
-      to_port     = 65535
-      type        = "ingress"
-      self        = true
-    }
-    # metrics-server
-    ingress_cluster_4443_webhook = {
-      description                   = "Cluster API to node 4443/tcp webhook"
-      protocol                      = "tcp"
-      from_port                     = 4443
-      to_port                       = 4443
-      type                          = "ingress"
-      source_cluster_security_group = true
-    }
-    # prometheus-adapter
-    ingress_cluster_6443_webhook = {
-      description                   = "Cluster API to node 6443/tcp webhook"
-      protocol                      = "tcp"
-      from_port                     = 6443
-      to_port                       = 6443
-      type                          = "ingress"
-      source_cluster_security_group = true
-    }
-    # Karpenter
-    ingress_cluster_8443_webhook = {
-      description                   = "Cluster API to node 8443/tcp webhook"
-      protocol                      = "tcp"
-      from_port                     = 8443
-      to_port                       = 8443
-      type                          = "ingress"
-      source_cluster_security_group = true
-    }
-    # ALB controller, NGINX
-    ingress_cluster_9443_webhook = {
-      description                   = "Cluster API to node 9443/tcp webhook"
-      protocol                      = "tcp"
-      from_port                     = 9443
-      to_port                       = 9443
-      type                          = "ingress"
-      source_cluster_security_group = true
-    }
-    egress_all = {
-      description      = "Allow all egress"
-      protocol         = "-1"
-      from_port        = 0
-      to_port          = 0
-      type             = "egress"
-      cidr_blocks      = ["0.0.0.0/0"]
-      ipv6_cidr_blocks = var.cluster_ip_family == "ipv6" ? ["::/0"] : null
-    }
-  } : k => v if var.node_security_group_enable_recommended_rules }
+  # node_security_group_recommended_rules = { for k, v in {
+  #   ingress_nodes_ephemeral = {
+  #     description = "Node to node ingress on ephemeral ports"
+  #     protocol    = "tcp"
+  #     from_port   = 1025
+  #     to_port     = 65535
+  #     type        = "ingress"
+  #     self        = true
+  #   }
+  #   # metrics-server
+  #   ingress_cluster_4443_webhook = {
+  #     description                   = "Cluster API to node 4443/tcp webhook"
+  #     protocol                      = "tcp"
+  #     from_port                     = 4443
+  #     to_port                       = 4443
+  #     type                          = "ingress"
+  #     source_cluster_security_group = true
+  #   }
+  #   # prometheus-adapter
+  #   ingress_cluster_6443_webhook = {
+  #     description                   = "Cluster API to node 6443/tcp webhook"
+  #     protocol                      = "tcp"
+  #     from_port                     = 6443
+  #     to_port                       = 6443
+  #     type                          = "ingress"
+  #     source_cluster_security_group = true
+  #   }
+  #   # Karpenter
+  #   ingress_cluster_8443_webhook = {
+  #     description                   = "Cluster API to node 8443/tcp webhook"
+  #     protocol                      = "tcp"
+  #     from_port                     = 8443
+  #     to_port                       = 8443
+  #     type                          = "ingress"
+  #     source_cluster_security_group = true
+  #   }
+  #   # ALB controller, NGINX
+  #   ingress_cluster_9443_webhook = {
+  #     description                   = "Cluster API to node 9443/tcp webhook"
+  #     protocol                      = "tcp"
+  #     from_port                     = 9443
+  #     to_port                       = 9443
+  #     type                          = "ingress"
+  #     source_cluster_security_group = true
+  #   }
+  #   egress_all = {
+  #     description      = "Allow all egress"
+  #     protocol         = "-1"
+  #     from_port        = 0
+  #     to_port          = 0
+  #     type             = "egress"
+  #     cidr_blocks      = ["0.0.0.0/0"]
+  #     ipv6_cidr_blocks = var.cluster_ip_family == "ipv6" ? ["::/0"] : null
+  #   }
+  # } : k => v if var.node_security_group_enable_recommended_rules }
 }
 
 # resource "aws_security_group" "node" {

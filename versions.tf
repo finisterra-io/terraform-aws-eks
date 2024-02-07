8b577617c76cdc36ea73ea12e545e400bde86a4c
Julian <eng@finisterra.io>
2024-02-06 23:21:24 -0500

    Update version requirements for terraform, aws, and providers

--- versions.tf
+++ versions.tf
@@ -6,5 +6,17 @@ terraform {
       source  = "hashicorp/aws"
       version = ">= 4.67"
     }
+    tls = {
+      source  = "hashicorp/tls"
+      version = ">= 3.0"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = ">= 2.10"
+    }
+    time = {
+      source  = "hashicorp/time"
+      version = ">= 0.9"
+    }
   }
 }

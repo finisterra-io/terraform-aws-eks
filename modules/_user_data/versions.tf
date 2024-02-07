05788b4122adfb74e11c083ebdf6d9b4b55245fb
Julian <eng@finisterra.io>
2024-02-06 21:48:46 -0500

    fix: versions

--- modules/_user_data/versions.tf
+++ modules/_user_data/versions.tf
@@ -1,10 +1,10 @@
 terraform {
-  required_version = ">= 1.0"
+  required_version = ">= 1.5.0"
 
   required_providers {
-    cloudinit = {
-      source  = "hashicorp/cloudinit"
-      version = ">= 2.0"
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 4.67"
     }
   }
 }
